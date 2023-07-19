c7b1e808762c08f9d50d5fb904ad3e113496dda3
Joohyoung Kim <silentsky@hotmail.com>
2023-07-19 07:17:28 +0600

    Add users page
    Add user list page and mui data grid componet.

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,8 +1,15 @@
+import DataTable from "../../components/dataTable/dataTable"
 import "./Users.scss"
 
 const Users = () => {
     return (
-        <div className="users">Users</div>
+        <div className="users">
+            <div className="info">
+                <h1>Users</h1>
+                <button>Add New User</button>
+            </div>
+            <DataTable/>
+        </div>
     )
 }
 
